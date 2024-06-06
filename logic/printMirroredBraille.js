b00f45f0f4fa8417b3d83683e6fdb8b1d0e0dd25
jrmth24 <jorman240802@gmail.com>
2024-06-06 17:40:38 -0500

    Add files via upload

---
+++ logic/printMirroredBraille.js
@@ -0,0 +1,11 @@
+//Listener para darle accion al boton
+document.getElementById('saveTXTButton').addEventListener('click', function() {
+    printMirroredBraille(document.getElementById('brailleOutput').textContent);
+});
+
+//Funcion para preparar la impresion
+function printMirroredBraille(textToPrint) {
+    localStorage.setItem('translatedText', textToPrint); // Almacenar en localStorage
+    window.location.href = './print.html'; // Cambiar a la página de destino
+    
+}
